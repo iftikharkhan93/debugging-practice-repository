efc8b5deb3ae2ad0f99208092142de8f5a19f72d
iftikharkhan93 <iftikhar.stockerz@gmail.com>
2020-12-05 18:47:48 -0500

    Add files via upload

---
+++ debugging-practice-master/exercise/scripts/main.js
@@ -0,0 +1,55 @@
+window.addEventListener('DOMContentLoaded', function() {
+  var cards = document.querySelectorAll('.card');
+  var selectedCards = [];
+  var matchedCards = [];
+  console.log('Printing the cards', cards);
+  console.log('Printing the selected cards', selectedCards);
+  console.log('Printing the matched cards', matchedCards);
+
+  cards.forEach(function(card) {
+    console.log('Printing the card', card);
+      card.addEventListener('click', function() {
+        console.log('clicking the card', card);
+
+      // If the card has already been matched, ignore it.
+      if (card.classList.contains('is-matched')) {
+        return;
+      }
+
+      // If we haven't selected 2 cards yet, add the current card to the
+      // collection of selected cards and apply the correct CSS class.
+      if (selectedCards.length < 2) {
+        card.classList.add('is-selected');
+        selectedCards.push(card);
+      }
+
+      // If we have selected two cards, see if they match.
+      if (selectedCards.length === 2) {
+         console.log('no of selected cards is 2');
+        var card1 = selectedCards[0];
+        var card2 = selectedCards[1];
+
+        // If the cards match, add them to the collection of matched cards and
+        // apply the correct CSS class.
+        if (card1.innerText === card2.innerText) {
+           console.log('print if both card 1 & 2 matches')
+          matchedCards.push(card1, card2);
+          card1.classList.add('is-matched');
+          card2.classList.add('is-matched');
+        }
+
+        // Regardless of whether or not the cards match, deselect them and reset
+        // the collection of matched cards.
+        card1.classList.remove('is-selected');
+        card2.classList.remove('is-selected');
+        selectedCards = [];
+      }
+
+      // If we've matched all the cards, display a message.
+      if (matchedCards.length >= cards.length) {
+        alert('You matched all the cards, nice job!');
+        
+      }
+    });
+  });
+});
